4e27a93dd38871bdfa20586a4062e34835655d71
Rodin-lee <kyeongbong.lee@gmail.com>
2017-08-02 08:39:09 +0900

    Update SimpleArraySum.py

--- Algorithms/Warmup-Challenges/SimpleArraySum.py
+++ Algorithms/Warmup-Challenges/SimpleArraySum.py
@@ -1,1 +1,21 @@
 # Simple Array Sum
+
+# Solution 1
+
+n = int(input().strip())
+arr = list(map(int, input().strip().split()))
+
+def ArraySum(n, ar):
+  sum = 0
+  for elem in range(n):
+    sum += arr[elem]
+  return sum
+
+result = SimpleArray(n, arr)
+print(result)
+
+# Solution 2
+
+n = int(input().strip())
+arr = list(map(int, input.split()))
+print(sum(arr))
